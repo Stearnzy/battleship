99c31e5154358386ea29f2ad1751ab2da13256ce
Zach Stearns <zach.stearns92@gmail.com>
2020-08-25 13:01:05 -0600

    Add cells and valid_coordinates? methods

--- lib/board.rb
+++ lib/board.rb
@@ -1,7 +1,26 @@
+require './lib/cell'
+
 class Board
-  attr_reader :cells
+  attr_reader :coordinates
 
   def initialize
+    @cell_names = ["A1", "A2", "A3", "A4", "B1", "B2", "B3", "B4",
+    "C1", "C2", "C3", "C4", "D1", "D2", "D3", "D4"]
+    @coordinates = {}
+  end
 
+  def cells
+    @coordinates = Hash[@cell_names.collect do |name|
+      [name, Cell.new(name)]
+    end]
+    # p coordinates
+  end
+
+  def valid_coordinate?(key)
+    if self.coordinates.has_key?(key)
+      return true
+    else
+      return false
+    end
   end
 end
