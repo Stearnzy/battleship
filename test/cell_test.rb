3dcec8ecba5a2c75453968452132c98c3f3b01ad
Nick King <nickmaxking@gmail.com>
2020-08-24 21:26:58 -0600

    Add render test

--- test/cell_test.rb
+++ test/cell_test.rb
@@ -49,4 +49,20 @@ class CellTest < Minitest::Test
     assert_equal 2, cell.ship.health
   end
 
+  def test_it_can_render
+    cell_1 = Cell.new("B4")
+
+    assert_equal ".", cell_1.render
+
+    cell_1.fire_upon
+
+    assert_equal "M", cell_1.render
+
+    cell_2 = Cell.new("C3")
+    cruiser = Ship.new("Cruiser", 3)
+    cell_2.place_ship(cruiser)
+
+    assert_equal ".", cell_2.render
+  end
+
 end
