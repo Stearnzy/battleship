03150c7a58c36bca9bf334d7d186e20cd4efeed7
Zach Stearns <zach.stearns92@gmail.com>
2020-08-25 13:01:39 -0600

    Add tests for board existence, cell existence, and validating coordinates

--- test/board_test.rb
+++ test/board_test.rb
@@ -1,5 +1,26 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/ship'
+require './lib/cell'
 require './lib/board'
 
 class BoardTest < Minitest::Test
 
+  def test_it_exists
+    board = Board.new
+    assert_instance_of Board, board
+  end
+
+  def test_cells_exist
+    board = Board.new
+    board.cells
+    assert_equal 16, board.coordinates.keys.length
+  end
+
+  def test_validate_coordinates
+    board = Board.new
+    board.cells
+    assert board.valid_coordinate?("A1")
+    refute board.valid_coordinate?("E3")
+  end
 end
