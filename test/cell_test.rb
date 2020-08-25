99557e37755bc82c0f8b95989bc44e83f8626931
Zach Stearns <zach.stearns92@gmail.com>
2020-08-24 20:35:34 -0600

    Change setup class to hardcoded tests

--- test/cell_test.rb
+++ test/cell_test.rb
@@ -4,51 +4,49 @@ require './lib/ship'
 require './lib/cell'
 
 class CellTest < Minitest::Test
-  def setup
-    @cell = Cell.new("B4")
-    @cruiser = Ship.new("Cruiser", 3)
-  end
 
   def test_it_exists
-
-    assert_instance_of Cell, @cell
+    cell = Cell.new("B4")
+    cruiser = Ship.new("Cruiser", 3)
+    assert_instance_of Cell, cell
   end
 
   def test_it_has_readable_attributes
-
-    assert_equal "B4", @cell.coordinate
-    assert_equal nil, @cell.ship
+    cell = Cell.new("B4")
+    cruiser = Ship.new("Cruiser", 3)
+    assert_equal "B4", cell.coordinate
+    assert_nil cell.ship
   end
 
   def test_if_it_can_place_ship_into_cell
-
-    assert_equal nil, @cell.ship
-
-    @cell.place_ship(@cruiser)
-
-    assert_equal @cruiser, @cell.ship
+    cell = Cell.new("B4")
+    cruiser = Ship.new("Cruiser", 3)
+    assert_nil cell.ship
+    cell.place_ship(cruiser)
+    assert_equal cruiser, cell.ship
   end
 
   def test_if_it_is_empty
+    cell = Cell.new("B4")
+    cruiser = Ship.new("Cruiser", 3)
+    assert_equal true, cell.empty?
 
-    assert_equal true, @cell.empty?
-
-    @cell.place_ship(@cruiser)
-
-    assert_equal false, @cell.empty?
+    cell.place_ship(cruiser)
+    assert_equal false, cell.empty?
   end
 
   def test_it_can_be_fired_upon
+    cell = Cell.new("B4")
+    cruiser = Ship.new("Cruiser", 3)
+    cell.place_ship(cruiser)
+    assert_equal false, cell.fired_upon?
+    assert_equal 3, cell.ship.health
 
-    @cell.place_ship(@cruiser)
-    assert_equal false, @cell.fired_upon?
-    assert_equal 3, @cell.ship.health
+    cell.fire_upon
 
-    @cell.fire_upon
-
-    assert_equal true, @cell.fired_upon?
-    @cell.ship.hit
-    assert_equal 2, @cell.ship.health
+    assert_equal true, cell.fired_upon?
+    cell.ship.hit
+    assert_equal 2, cell.ship.health
   end
 
 end
