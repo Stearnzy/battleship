91158b1abb90b0a014cc68cd7a5016373321ed52
Nick King <nickmaxking@gmail.com>
2020-08-25 20:09:34 -0600

    Add tests for validating ship placement

--- test/board_test.rb
+++ test/board_test.rb
@@ -8,19 +8,59 @@ class BoardTest < Minitest::Test
 
   def test_it_exists
     board = Board.new
+
     assert_instance_of Board, board
   end
 
   def test_cells_exist
     board = Board.new
     board.cells
+
     assert_equal 16, board.coordinates.keys.length
   end
 
-  def test_validate_coordinates
+  def test_it_can_validate_coordinates
     board = Board.new
     board.cells
+
     assert board.valid_coordinate?("A1")
     refute board.valid_coordinate?("E3")
   end
+
+  def test_it_does_not_validate_incorrect_ship_lengths
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    submarine = Ship.new("Submarine", 2)
+
+    refute board.valid_placement?(cruiser, ["A1", "A2"])
+    refute board.valid_placement?(submarine, ["A2", "A3", "A4"])
+  end
+
+  def test_it_does_not_validate_non_consecutive_coordinates
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    submarine = Ship.new("Submarine", 2)
+
+    refute board.valid_placement?(cruiser, ["A1", "A2", "A4"])
+    refute board.valid_placement?(submarine, ["A1", "C1"])
+    refute board.valid_placement?(cruiser, ["A3", "A2", "A1"])
+    refute board.valid_placement?(submarine, ["C1", "B1"])
+  end
+
+  def test_it_does_not_validate_diagonal_coordinates
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    submarine = Ship.new("Submarine", 2)
+
+    refute board.valid_placement?(cruiser, ["A1", "B2", "C3"])
+    refute board.valid_placement?(submarine, ["C2", "D3"])
+  end
+
+  def test_it_can_validate_proper_placement
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    submarine = Ship.new("Submarine", 2)
+
+    assert board.valid_placement?(submarine, ["A1", "A2"])
+    assert board.valid_placement?(cruiser, ["B1", "C1", "D1"])
 end
