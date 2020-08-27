4a3d9682b5be500041cb7ea516dcf9de28e13af8
Nick King <nickmaxking@gmail.com>
2020-08-27 14:47:09 -0600

    Initial commit, add test it exists

--- test/game_test.rb
+++ test/game_test.rb
@@ -1,1 +1,13 @@
- 
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/ship'
+require './lib/cell'
+require './lib/board'
+require './lib/game'
+
+class GameTest < Minitest::Test
+  def test_it_exists
+    game = Game.new
+    assert_instance_of Game, game
+  end
+end
