d3d08456c94644a9fc2f25037b02dc291251a2ab
Nick King <nickmaxking@gmail.com>
2020-08-26 21:14:10 -0600

    Add tests for rendering hits, misses and sinks

--- test/board_test.rb
+++ test/board_test.rb
@@ -90,7 +90,6 @@ class BoardTest < Minitest::Test
   end
 
   def test_it_can_render_empty_board
-    skip
     board = Board.new
 
     assert_equal "  1 2 3 4 \n" +
@@ -101,9 +100,9 @@ class BoardTest < Minitest::Test
   end
 
   def test_it_can_render_ship_on_board
-    skip
     board = Board.new
     cruiser = Ship.new("Cruiser", 3)
+
     board.place(cruiser, ["A1", "A2", "A3"])
 
     assert_equal "  1 2 3 4 \n" +
@@ -111,5 +110,78 @@ class BoardTest < Minitest::Test
                 "B . . . . \n" +
                 "C . . . . \n" +
                 "D . . . . \n", board.render(true)
+  end
+
+  def test_it_can_render_a_miss
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    board.place(cruiser, ["A4", "B4", "C4"])
+
+    assert_equal "  1 2 3 4 \n" +
+                "A . . . S \n" +
+                "B . . . S \n" +
+                "C . . . S \n" +
+                "D . . . . \n", board.render(true)
+
+    board.cells["D4"].fire_upon
+    assert_equal "  1 2 3 4 \n" +
+                "A . . . . \n" +
+                "B . . . . \n" +
+                "C . . . . \n" +
+                "D . . . M \n", board.render
+    assert_equal "  1 2 3 4 \n" +
+                "A . . . S \n" +
+                "B . . . S \n" +
+                "C . . . S \n" +
+                "D . . . M \n", board.render(true)
+  end
+
+  def test_if_can_render_a_hit
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    board.place(cruiser, ["A4", "B4", "C4"])
+
+    board.cells["A4"].fire_upon
+    assert_equal "  1 2 3 4 \n" +
+                 "A . . . H \n" +
+                 "B . . . . \n" +
+                 "C . . . . \n" +
+                 "D . . . . \n", board.render
+    assert_equal "  1 2 3 4 \n" +
+                 "A . . . H \n" +
+                 "B . . . S \n" +
+                 "C . . . S \n" +
+                 "D . . . . \n", board.render(true)
+  end
+
+  def test_it_can_render_a_sink
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    board.place(cruiser, ["A4", "B4", "C4"])
+
+    board.cells["A4"].fire_upon
+    board.cells["B4"].fire_upon
+    assert_equal "  1 2 3 4 \n" +
+                 "A . . . H \n" +
+                 "B . . . H \n" +
+                 "C . . . . \n" +
+                 "D . . . . \n", board.render
+    assert_equal "  1 2 3 4 \n" +
+                 "A . . . H \n" +
+                 "B . . . H \n" +
+                 "C . . . S \n" +
+                 "D . . . . \n", board.render(true)
+
+    board.cells["C4"].fire_upon
+    assert_equal "  1 2 3 4 \n" +
+                 "A . . . X \n" +
+                 "B . . . X \n" +
+                 "C . . . X \n" +
+                 "D . . . . \n", board.render
+    assert_equal "  1 2 3 4 \n" +
+                 "A . . . X \n" +
+                 "B . . . X \n" +
+                 "C . . . X \n" +
+                 "D . . . . \n", board.render(true)
   end
 end
