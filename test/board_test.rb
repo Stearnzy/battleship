3afa4e7ea5a52866d733bd545a6f070b41a7687e
Nick King <nickmaxking@gmail.com>
2020-08-26 18:31:30 -0600

    Add test for overlapping ships. Reformat render tests

--- test/board_test.rb
+++ test/board_test.rb
@@ -73,25 +73,43 @@ class BoardTest < Minitest::Test
     cell_1 = board.cells["A1"]
     cell_2 = board.cells["A2"]
     cell_3 = board.cells["A3"]
+    cell_4 = board.cells["A4"]
 
     assert_equal cruiser, cell_1.ship
     assert_equal cruiser, cell_2.ship
     assert_equal cruiser, cell_3.ship
+    assert_equal nil, cell_4.ship
   end
 
-  def it_can_render_empty_board
+  def test_ships_cannot_overlap
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    board.place(cruiser, ["A1", "A2", "A3"])
+    submarine = Ship.new("Submarine", 2)
+    assert_equal false, board.valid_placement?(submarine, ["A1", "B1"])
+  end
+
+  def test_it_can_render_empty_board
     skip
     board = Board.new
 
-    assert_equal "  1 2 3 4 \nA . . . . \nB . . . . \nC . . . . \nD . . . . \n", board.render
+    assert_equal "  1 2 3 4 \n" +
+                "A . . . . \n" +
+                "B . . . . \n" +
+                "C . . . . \n" +
+                "D . . . . \n", board.render
   end
 
-  def it_can_render_ship_on_board
+  def test_it_can_render_ship_on_board
     skip
     board = Board.new
     cruiser = Ship.new("Cruiser", 3)
     board.place(cruiser, ["A1", "A2", "A3"])
 
-    assert_equal "  1 2 3 4 \nA S S S . \nB . . . . \nC . . . . \nD . . . . \n", board.render(true)
+    assert_equal "  1 2 3 4 \n" +
+                "A S S S . \n" +
+                "B . . . . \n" +
+                "C . . . . \n" +
+                "D . . . . \n", board.render(true)
   end
 end
