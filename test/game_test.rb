fb2bcc79a8228f63688146c7ad9dbc4ad8db470a
Zach Stearns <zach.stearns92@gmail.com>
2020-08-31 16:31:54 -0600

    WIP, trying to come up with the correct way to test

--- test/game_test.rb
+++ test/game_test.rb
@@ -10,4 +10,36 @@ class GameTest < Minitest::Test
     game = Game.new
     assert_instance_of Game, game
   end
+
+  # def test_game_is_still_going
+  #   game = Game.new
+  #   @player_board = Board.new
+  #   @computer_board = Board.new
+    # cell_1 = board.cells["A1"]
+    # cell_2 = board.cells["A2"]
+    # cell_3 = board.cells["A3"]
+  # end
+
+  def test_computer_ship_placement
+    skip
+    # WTF!!!! Undefined method for cell_names in line 157
+    game = Game.new
+    @computer_board = Board.new
+    @player_board = Board.new
+    @computer_board.cells
+    game.computer_ship_placement
+    require "pry"; binding.pry
+    # assert_equal 5,
+  end
+
+  def test_player_cruiser_placement
+    skip
+    # Not sure if I can test this either, since this requires @cruiser_placement,
+    # which is defined in player_cruiser_validation_check by requiring user input.
+    game = Game.new
+    @player_board = Board.new
+    @cruiser_placement_entry = ["A1", "A2", "A3"]
+    game.player_place_cruiser
+
+  end
 end
