bc28f7bcc8564b6231060c952423eb576f6841a0
Zach Stearns <zach.stearns92@gmail.com>
2020-08-31 20:04:53 -0600

    WIP, refactoring

--- battleship_runner.rb
+++ battleship_runner.rb
@@ -2,7 +2,16 @@ require './lib/ship'
 require './lib/cell'
 require './lib/cell_generator'
 require './lib/board'
-require './lib/game'
+require './lib/gameplay'
+require './lib/game_setup'
 
-game = Game.new
-game.play
+# game = Game.new
+# game.play
+
+
+setup = GameSetup.new
+setup.setup
+gameplay = Game.new(setup.cell_names, setup.height, setup.width)
+# setup.placement
+# gameplay.computer_ship_placement
+gameplay.play
