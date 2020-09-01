a5fda8835dcf2c338cfd70ebfa1bc60266f31380
Zach Stearns <zach.stearns92@gmail.com>
2020-08-31 21:44:18 -0600

    Change requires and removed unneccessary arguments

--- test/gameplay_test.rb
+++ test/gameplay_test.rb
@@ -3,11 +3,13 @@ require 'minitest/pride'
 require './lib/ship'
 require './lib/cell'
 require './lib/board'
-require './lib/game'
+require './lib/cell_generator'
+require './lib/game_setup'
+require './lib/gameplay'
 
 class GamePlayTest < Minitest::Test
   def test_it_exists
-    gameplay = GamePlay.new
+    gameplay = GamePlay.new(:cell_names, setup.computer_cruiser, setup.computer_submarine, setup.player_place_cruiser, setup.player_submarine, setup.computer_board, setup.player_board)
     assert_instance_of GamePlay, gameplay
   end
 end
