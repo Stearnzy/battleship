6b34698fe55dc437bda5f41ce1bcd20c2ca27c18
Zach Stearns <zach.stearns92@gmail.com>
2020-08-31 21:46:41 -0600

    Removed computer and player ship arguments

--- test/gameplay_test.rb
+++ test/gameplay_test.rb
@@ -9,7 +9,7 @@ require './lib/gameplay'
 
 class GamePlayTest < Minitest::Test
   def test_it_exists
-    gameplay = GamePlay.new(:cell_names, setup.computer_cruiser, setup.computer_submarine, setup.player_place_cruiser, setup.player_submarine, setup.computer_board, setup.player_board)
+    gameplay = GamePlay.new(:cell_names, setup.computer_board, setup.player_board)
     assert_instance_of GamePlay, gameplay
   end
 end
