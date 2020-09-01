2a59fd239c2b13a1bcdc3bbb31a8bdbe959d1ccf
Zach Stearns <zach.stearns92@gmail.com>
2020-09-01 13:52:19 -0600

    Add tests for game goes on, computer can fire upon and sink ship, computer can win.

--- test/gameplay_test.rb
+++ test/gameplay_test.rb
@@ -9,42 +9,59 @@ require './lib/gameplay'
 
 class GamePlayTest < Minitest::Test
   def test_it_exists
-    # board_specs = CellGenerator.new(4, 4)
-    # board_specs.populate_cell_names
-    # player_board = Board.new(board_specs.cell_names, @height, @width)
-    # computer_board = Board.new(board_specs.cell_names, @height, @width)
-    # gameplay = GamePlay.new(board_specs.cell_names, computer_board, player_board)
     cell_names = ["A1"]
     player_board = Board.new(cell_names, 1, 1)
     computer_board = Board.new(cell_names, 1, 1)
     gameplay = GamePlay.new(cell_names, computer_board, player_board)
+
     assert_instance_of GamePlay, gameplay
   end
 
-  def test_computer_can_fire
-    skip
-    # Needs reworking...
-    cell_names = ["A1", "A2", "A3"]
-    player_board = Board.new(cell_names, 1, 3)
-    computer_board = Board.new(cell_names, 1, 3)
+  def test_game_goes_on_or_not
+    cell_names = ["A1", "A2"]
+    player_board = Board.new(cell_names, 1, 2)
+    computer_board = Board.new(cell_names, 1, 2)
     player_submarine = Ship.new("Submarine", 2)
-    # Running pry here shows only one cell when entering player_board.cells
-    require "pry"; binding.pry
+    player_board.place(player_submarine, ["A1", "A2"])
+    computer_submarine = Ship.new("Submarine", 2)
+    computer_board.place(computer_submarine, ["A1", "A2"])
+    gameplay = GamePlay.new(cell_names, computer_board, player_board)
 
+    assert gameplay.game_is_still_going?
+    gameplay.turn_computer_shot
+    gameplay.turn_computer_shot
+    refute gameplay.game_is_still_going?
+  end
+
+  def test_computer_can_fire_upon_and_sink_player_ship
+    cell_names = ["A1", "A2"]
+    player_board = Board.new(cell_names, 1, 2)
+    computer_board = Board.new(cell_names, 1, 2)
+    player_submarine = Ship.new("Submarine", 2)
     player_board.place(player_submarine, ["A1", "A2"])
     gameplay = GamePlay.new(cell_names, computer_board, player_board)
-    @computer_shot = "A1"
-    refute player_board.cells["A1"].fired_upon?
+
+    refute player_board.cells["A1"].fired_upon? && player_board.cells["A2"].fired_upon?
     gameplay.turn_computer_shot
-    assert player_board.cells["A1"].fired_upon?
+    assert player_board.cells["A1"].fired_upon? || player_board.cells["A2"].fired_upon?
+    gameplay.turn_computer_shot
+    assert player_board.cells["A1"].fired_upon? && player_board.cells["A2"].fired_upon?
+    assert player_submarine.sunk?
   end
 
   def test_computer_can_win
-    # Needs work too
-    cell_names = ["A1"]
-    player_board = Board.new(cell_names, 1, 1)
-    computer_board = Board.new(cell_names, 1, 1)
+    cell_names = ["A1", "A2"]
+    player_board = Board.new(cell_names, 1, 2)
+    computer_board = Board.new(cell_names, 1, 2)
+    player_submarine = Ship.new("Submarine", 2)
+    computer_submarine = Ship.new("Submarine", 2)
+    player_board.place(player_submarine, ["A1", "A2"])
+    computer_board.place(computer_submarine, ["A1", "A2"])
     gameplay = GamePlay.new(cell_names, computer_board, player_board)
-    @computer_shot = "A1"
+    gameplay.turn_computer_shot
+    gameplay.turn_computer_shot
+
+    assert_equal false, gameplay.victory
+    assert_equal false, gameplay.player_board.render(true).include?("S")
   end
 end
